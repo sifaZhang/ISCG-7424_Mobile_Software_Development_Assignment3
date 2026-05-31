d8435a21a3160050a0a43ff816abb51ba091ce85
AlternateDan <dan.man.saunders@gmail.com>
2026-05-31 14:14:23 +1200

    Removed user feedback, inquiry list in progress

--- Grabyourgear/app/src/main/java/com/group1/grabyourgear/utils/Adapter_InquiryView.java
+++ Grabyourgear/app/src/main/java/com/group1/grabyourgear/utils/Adapter_InquiryView.java
@@ -1,5 +1,56 @@
 package com.group1.grabyourgear.utils;
 
-public class Adapter_InquiryView {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.group1.grabyourgear.R;
+import com.group1.grabyourgear.models.Inquiry;
+
+import java.util.List;
+
+public class Adapter_InquiryView extends RecyclerView.Adapter<Adapter_InquiryView.InquiryViewHolder>{
     private List<Inquiry> inquiryList;
+    private Context context;
+
+    public Adapter_InquiryView(List<Inquiry> inquiryList, Context context){
+        this.inquiryList = inquiryList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public InquiryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_inquiry, parent, false);
+        return new InquiryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InquiryViewHolder holder, int position) {
+        Inquiry item = inquiryList.get(position);
+
+        holder.tvInquiryContact.setText(item.getContact());
+        holder.tvInquiryDetails.setText(item.getDetails());
+    }
+
+    @Override
+    public int getItemCount() {
+        return inquiryList.size();
+    }
+
+    public static class InquiryViewHolder extends RecyclerView.ViewHolder{
+        TextView tvInquiryContact, tvInquiryDetails;
+
+        public InquiryViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            tvInquiryContact = itemView.findViewById(R.id.tvInquiryContact);
+            tvInquiryDetails = itemView.findViewById(R.id.tvInquiryDetails);
+        }
+    }
 }
--- Grabyourgear/app/src/main/java/com/group1/grabyourgear/utils/FirebaseHelper_Inquiries.java
+++ Grabyourgear/app/src/main/java/com/group1/grabyourgear/utils/FirebaseHelper_Inquiries.java
@@ -1,4 +1,37 @@
 package com.group1.grabyourgear.utils;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.group1.grabyourgear.common.FirebaseNodes;
+import com.group1.grabyourgear.models.Inquiry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FirebaseHelper_Inquiries {
+    private static final DatabaseReference INQUIRY_REF =
+            FirebaseDatabase.getInstance().getReference(FirebaseNodes.BOOKINGS);
+
+    public interface InquiryListCallback {
+        void onSuccess(List<Inquiry> inquiryList);
+        void onFailure(Exception e);
+    }
+
+    public static void loadAllInquiries(InquiryListCallback callback) {
+        INQUIRY_REF.get().addOnSuccessListener(snapshot -> {
+
+            List<Inquiry> list = new ArrayList<>();
+            for (DataSnapshot child : snapshot.getChildren()) {
+                Inquiry item = child.getValue(Inquiry.class);
+                if (item != null) {
+                    item.setId(child.getKey());
+                    list.add(item);
+                }
+            }
+
+            callback.onSuccess(list);
+
+        }).addOnFailureListener(callback::onFailure);
+    }
 }
